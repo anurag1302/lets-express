db70b965d3008d7f26915e380cdd857b2c90b166
anurag1302 <anurag3487@gmail.com>
2025-09-07 22:00:05 +0530

    request params in get

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, username: "anurag", department: "IT" },
+  { id: 2, username: "john", department: "Finance" },
+  { id: 3, username: "jack", department: "HR" },
+];
+
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
@@ -13,11 +19,21 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, username: "anurag", department: "IT" },
-    { id: 2, username: "john", department: "Finance" },
-    { id: 3, username: "jack", department: "HR" },
-  ]);
+  response.send(users);
+});
+
+//Request Params
+app.get("/api/users/:id", (request, response) => {
+  const params = request.params;
+  const id = parseInt(params.id);
+  if (isNaN(id)) {
+    return response.status(400).send({ message: "Bad Request" });
+  }
+  const findUser = users.find((user) => user.id === id);
+  if (!findUser) {
+    return response.status(404).send({ message: "User Not Found" });
+  }
+  response.send(findUser);
 });
 
 app.get("/api/products", (request, response) => {
