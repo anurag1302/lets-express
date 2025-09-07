80fa8936fbc7b73716280db922845ff575092f57
anurag1302 <anurag3487@gmail.com>
2025-09-07 21:24:15 +0530

    get calls with static jsons

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
+
+app.get("/", (request, response) => {
+  response.status(200).send({ message: "Hello Express JS" });
+});
+
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "anurag", department: "IT" },
+    { id: 2, username: "john", department: "Finance" },
+    { id: 3, username: "jack", department: "HR" },
+  ]);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([
+    { id: 110, name: "CPU", price: 100 },
+    { id: 112, name: "mouse", price: 25 },
+  ]);
+});
